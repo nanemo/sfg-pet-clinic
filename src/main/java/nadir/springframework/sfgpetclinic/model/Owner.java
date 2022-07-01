fae6ce01f60a2d179d860475d3b4d5244408fe6c
Nadir Nadirli <companynadirnadirli@gmail.com>
2022-07-01 22:08:43 +0400

    adding POJOs. Closes #2.

---
+++ src/main/java/nadir/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package nadir.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/nadir/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package nadir.springframework.sfgpetclinic.model;
+
+public class Vet extends Person{
+
+}
